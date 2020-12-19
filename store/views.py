97bdea4239608c1b8ce0f75815adbee27fa49f43
Tanmay Ambadkar <tanmay.ambadkar@gmail.com>
2020-12-20 00:48:24 +0530

    Changing the method to get count of certificates

--- store/views.py
+++ store/views.py
@@ -155,7 +155,7 @@ class CertficateCount(APIView):
 
     def get(self, request, format=None):
 
-        certificates = Certificate.objects.all()
+        certificates = Certificate.objects.order_by('id')
         int(certificates[-1].id[-5:])
         return Response({"count":int(certificates[-1].id[-5:])+1})
 
