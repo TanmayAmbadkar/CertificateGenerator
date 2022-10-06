e7805eaa8866301f45d51e386281da551895541c
Krishna Gaur <gaurkrishna498@gmail.com>
2022-10-07 00:10:28 +0530

    added urls for reset password and forgot password

--- api/urls.py
+++ api/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
         path('upload', UploadCertificates.as_view(), name='upload_cert'),
         path('login', LoginTokenView.as_view(), name='login_token'),
         path('logout', LogoutTokenView.as_view(), name='logout_token'),
+        path('password-reset-email', RequestPasswordResetEmail.as_view(), name='password-reset-email'),
+        path('reset-password', PasswordResetConfirm.as_view(), name='reset-password')
 ]
