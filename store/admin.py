60c0b3f96fa00c2857a1bf9d469de6a7ed2a7d95
Krishna Gaur <gaurkrishna498@gmail.com>
2022-10-07 11:31:51 +0530

    Added list display and search fields to CertificateAdmin

--- store/admin.py
+++ store/admin.py
@@ -2,5 +2,16 @@ from django.contrib import admin
 from store.models import *
 # Register your models here.
 
+class CertificateAdmin(admin.ModelAdmin):
+    list_display = (
+        "Cert id",
+        "Rollno",
+        "Event",
+        "Date",
+        "Year"
+    )
+
+    search_fields = ("Rollno", "Event", "Cert id", "Year")
+
 admin.site.register(Certificate)
 admin.site.register(TempCert)
